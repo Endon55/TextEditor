66e7b794cac7888f88620c7f6da36d670a721b59
Anthony Cosenza <anthony@anthonycosenza.com>
2018-04-16 12:05:35 -0600

    Added a text field to the window.

--- src/com/cosenza/Main.java
+++ src/com/cosenza/Main.java
@@ -3,10 +3,15 @@ package com.cosenza;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
+/*
+    The goal of this project is to build a basic text editor.
+    Features I would like to add, Open, Save, Save As, Fonts, Size, Colors..........
+ */
 public class Main extends Application
 {
 
@@ -19,8 +24,15 @@ public class Main extends Application
     public void start(Stage primaryStage) throws Exception
     {
         primaryStage.setTitle("My first JavaFX app");
+
+        TextArea textArea = new TextArea();
+        VBox vbox = new VBox(textArea);
+        Scene scene = new Scene(vbox, 200, 100);
+
+        textArea.setMaxSize(5000,1000000);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
-        System.out.println(" ");
     }
 
 
