67147419f0414beb95c62db413a9b81e6ab0d52e
Anthony Cosenza <anthony@anthonycosenza.com>
2018-04-17 11:07:50 -0600

    Menus Fleshed out

--- src/com/cosenza/Window.java
+++ src/com/cosenza/Window.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -26,20 +27,43 @@ public class Window extends Application
     {
         primaryStage.setTitle("My first JavaFX app");
 
-        //Menu
         border = new BorderPane();
+        scene = new Scene(border);
+
+        //Menu
         MenuBar menuBar = new MenuBar();
+            //File
         Menu fileMenu = new Menu("File");
-        Menu optionsMenu = new Menu("Options");
+        MenuItem open = new MenuItem("Open");
+        MenuItem save = new MenuItem("Save");
+        MenuItem saveAs = new MenuItem("Save As");
+        MenuItem exit = new MenuItem("Exit");
+        fileMenu.getItems().addAll(open, save, saveAs, exit);
+            //Edit
+        Menu editMenu = new Menu("Edit");
+        MenuItem cut = new MenuItem("Cut");
+        MenuItem copy = new MenuItem("Copy");
+        MenuItem paste = new MenuItem("Paste");
+        editMenu.getItems().addAll(cut, copy, paste);
+            //Format
+        Menu formatMenu = new Menu("Format");
+        MenuItem size = new MenuItem("Size");
+        MenuItem font = new MenuItem("Font");
+        MenuItem color = new MenuItem("Color");
+        formatMenu.getItems().addAll(size, font, color);
+            //Help
         Menu helpMenu = new Menu("Help");
-        menuBar.getMenus().addAll(fileMenu, optionsMenu, helpMenu);
+        MenuItem version = new MenuItem("Version");
+        helpMenu.getItems().addAll(version);
+
+
+        menuBar.getMenus().addAll(fileMenu, editMenu, formatMenu, helpMenu);
         border.setTop(menuBar);
 
         //Text Area
         textArea = new TextArea();
         border.setCenter(textArea);
 
-        scene = new Scene(border);
         primaryStage.setScene(scene);
         primaryStage.sizeToScene();
         primaryStage.show();
